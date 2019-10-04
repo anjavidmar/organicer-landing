2e0a013e454c7df52a343c586619f3968e42a971
A Vidmar <a.v.vidmar@gmail.com>
2019-10-04 18:00:03 +0200

    new tem photos and fix up to 500px width

--- js/script.js
+++ js/script.js
@@ -3,17 +3,21 @@ var header = document.getElementById('header');
 var header_h = 0;
 var headerHero = document.querySelector('.header__hero');
 var headerHero_h = 0;
+var headerHeroImg = document.getElementById('header-hero');
 var headerGreen = document.querySelector('.header__green');
 var headerGreen_h = headerGreen.offsetHeight;
 var imageContainerInner = document.querySelector('.image-container--inner');
 var imageContainerInner_h = imageContainerInner.offsetHeight;
 var mainHero = document.querySelector('.main__hero');
 var mainHero_h = imageContainerInner_h * 1.5;
+var mainHeroImg = document.getElementById('main-hero');
 var screen = document.getElementById('screen');
 var w = window.innerWidth;
 
 console.log('hello world!', w, h, header, headerHero, headerHero_h, headerGreen, headerGreen_h);
 console.log(imageContainerInner, imageContainerInner_h, mainHero_h);
+console.log(headerHeroImg);
+console.log(mainHeroImg);
 
 if (h < 400) {
     header_h = h + headerGreen_h;
@@ -23,9 +27,13 @@ if (h < 400) {
     headerHero_h = h - headerGreen_h;
 }
 
-console.log('1 : ', h);
-console.log('2 : ', header_h);
-console.log('3 : ', headerHero_h);
+if (w > 584) {
+    headerHeroImg.src = './assets/images/header-hero.jpg';
+    mainHero_h = imageContainerInner_h * 1.25;
+}
+
+console.log('screen height: ', h);
+console.log('screen width: ', w);
 
 header.style.minHeight = header_h + 'px';
 headerHero.style.height = headerHero_h + 'px';
