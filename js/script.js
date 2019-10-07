5d20c23315a2084de15542364e304f03a9844cd1
A Vidmar <a.v.vidmar@gmail.com>
2019-10-07 14:13:51 +0200

    large screen sizes fix

--- js/script.js
+++ js/script.js
@@ -6,18 +6,15 @@ var headerHero_h = 0;
 var headerHeroImg = document.getElementById('header-hero');
 var headerGreen = document.querySelector('.header__green');
 var headerGreen_h = headerGreen.offsetHeight;
-var imageContainerInner = document.querySelector('.image-container--inner');
+var imageContainerInner = document.querySelector('.image-container__inner');
 var imageContainerInner_h = imageContainerInner.offsetHeight;
 var mainHero = document.querySelector('.main__hero');
 var mainHero_h = imageContainerInner_h * 1.5;
 var mainHeroImg = document.getElementById('main-hero');
+var bestNewsText = document.querySelector('section:nth-child(12) h3');
+var elevateText = document.querySelector('section:nth-child(5) h3');
 var screen = document.getElementById('screen');
 var w = window.innerWidth;
-
-console.log('hello world!', w, h, header, headerHero, headerHero_h, headerGreen, headerGreen_h);
-console.log(imageContainerInner, imageContainerInner_h, mainHero_h);
-console.log(headerHeroImg);
-console.log(mainHeroImg);
 
 if (h < 400) {
     header_h = h + headerGreen_h;
@@ -30,12 +27,10 @@ if (h < 400) {
 if (w > 584) {
     headerHeroImg.src = './assets/images/header-hero.jpg';
     mainHero_h = imageContainerInner_h * 1.25;
+    bestNewsText.innerHTML = "And the best news is,<br /> WE'RE GIVING IT TO YOU"
+    elevateText.innerHTML = 'Elevate your task management to a whole<br /> new level with our 2020 Organicer.';
 }
-
-console.log('screen height: ', h);
-console.log('screen width: ', w);
 
 header.style.minHeight = header_h + 'px';
 headerHero.style.height = headerHero_h + 'px';
 mainHero.style.height = mainHero_h + 'px';
-screen.innerHTML = 'screen width: ' + w + ', screen height: ' + h;
